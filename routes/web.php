e916a081c0541f8bd7fb7b3240c97be8a5b35bbd
syava aprilia puspitasari <syafaaps3@gmail.com>
2024-09-06 10:51:30 +0700

    Jobsheet 2 Praltikum 1

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Htpp\Controllers\PhotoController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::resource ('photos', App\Http\Controllers\PhotoController:: class);
+Route::resource ('photos', App\Http\Controllers\PhotoController:: class) ->only (['index', 'show']);
+Route::resource ('photos', App\Http\Controllers\PhotoController:: class) ->except(['create', 'store', 'update', 'destroy']);
 
-Route::get('/', function () {
-    return view('welcome');
+// MODIF
+Route::get('/homepage', [App\Http\Controllers\PageController::class,'home']);
+Route::get('/aboutpage', [App\Http\Controllers\PageController::class,'about']);
+Route::get('/articlespage/{id}', [App\Http\Controllers\PageController::class,'articles']);
+//
+
+Route::get('/index', [App\Http\Controllers\PageController::class,'index']);
+
+Route::get('/about', [App\Http\Controllers\PageController::class,'about']);
+
+Route::get('/articles', [App\Http\Controllers\PageController::class,'articles']);
+
+
+Route::get('/hello', [App\Http\Controllers\WelcomeController::class,'hello']);
+
+Route::get('/user/{name?}', function ($name='John') { 
+    return 'Nama saya '.$name; 
 });
+
+
